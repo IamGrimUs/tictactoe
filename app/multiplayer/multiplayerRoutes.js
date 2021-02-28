c0cfd11ed6b9829311629811329493ad858a87de
r-dbl-l <iamgrimus@gmail.com>
2021-02-28 10:37:57 -0800

    route update

--- app/multiplayer/multiplayerRoutes.js
+++ app/multiplayer/multiplayerRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: false }))
 
-router.post('/game', multiplayerController.startGame) // check db for records if record exists with no playerTwo add playerTwo or if no records then create game in db
+router.post('/', multiplayerController.startGame) // check db for records if record exists with no playerTwo add playerTwo or if no records then create game in db
 // router.put('/game/:id', multiplayerController.playerMove) // sending player selection to game record with matching ID then checking for win conditions
 // router.get('/game/:id', multiplayerController.getGame) // constantly return game record
 
