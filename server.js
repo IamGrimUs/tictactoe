d88d50ac2f9575664da57a39ba2a191995b36bfc
r-dbl-l <iamgrimus@gmail.com>
2021-02-28 09:32:39 -0800

    fixing misspelling

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const cors = requrie('cors')
+const cors = require('cors')
 
 const multiplayerRoutes = require('./app/multiplayer/multiplayerRoutes')
 
